5ab0300b9a2650a56173e8012f8069499a3bdca3
David Lazar <david.lazar@sap.com>
2019-06-27 16:14:52 +0200

    Logo change and Add remove test

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 include_once 'config.php';
 include_once 'include/Webservices/Relation.php';
 
-include_once 'vtlib/Vtiger/Module.php';
+//include_once 'vtlib/Vtiger/Module.php';
 include_once 'includes/main/WebUI.php';
 
 $webUI = new Vtiger_WebUI();
