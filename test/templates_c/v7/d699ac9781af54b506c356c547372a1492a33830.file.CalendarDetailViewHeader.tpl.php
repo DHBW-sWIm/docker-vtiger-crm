49bc3e0cb7a34ca3a0cc56dc487defd3a17c63f3
sWIm-crm <sWIm-crm@protonmail.com>
2019-07-20 12:12:05 +0000

    Config backup

---
+++ test/templates_c/v7/d699ac9781af54b506c356c547372a1492a33830.file.CalendarDetailViewHeader.tpl.php
@@ -0,0 +1,29 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-07-20 12:11:05
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Users/CalendarDetailViewHeader.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:21162738475d3304d9a635e5-04331652%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'd699ac9781af54b506c356c547372a1492a33830' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Users/CalendarDetailViewHeader.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '21162738475d3304d9a635e5-04331652',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'MODULE_MODEL' => 0,
+    'RECORD' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5d3304d9a6d7b',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5d3304d9a6d7b')) {function content_5d3304d9a6d7b($_smarty_tpl) {?>
+<?php $_smarty_tpl->tpl_vars["MODULE_NAME"] = new Smarty_variable($_smarty_tpl->tpl_vars['MODULE_MODEL']->value->get('name'), null, 0);?><input id="recordId" type="hidden" value="<?php echo $_smarty_tpl->tpl_vars['RECORD']->value->getId();?>
+" /><div class="detailViewContainer"><div class="detailViewTitle" id="prefPageHeader"></div><div class="detailViewInfo userPreferences row-fluid"><div class="details col-xs-12"><?php }} ?>
---
+++ test/templates_c/v7/fd6b040c5258bd456119b47fd3bb243108917fc4.file.CalendarDetailViewPreProcess.tpl.php
@@ -0,0 +1,30 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2019-07-20 12:11:05
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Users/CalendarDetailViewPreProcess.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:19075617485d3304d9924042-83876507%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'fd6b040c5258bd456119b47fd3bb243108917fc4' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Users/CalendarDetailViewPreProcess.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '19075617485d3304d9924042-83876507',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'QUALIFIED_MODULE' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5d3304d99a451',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5d3304d99a451')) {function content_5d3304d99a451($_smarty_tpl) {?>
+
+<?php echo $_smarty_tpl->getSubTemplate (vtemplate_path("SettingsMenuStart.tpl",$_smarty_tpl->tpl_vars['QUALIFIED_MODULE']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+<div class="bodyContents"><div><?php echo $_smarty_tpl->getSubTemplate (vtemplate_path("CalendarDetailViewHeader.tpl",$_smarty_tpl->tpl_vars['QUALIFIED_MODULE']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+<?php }} ?>
