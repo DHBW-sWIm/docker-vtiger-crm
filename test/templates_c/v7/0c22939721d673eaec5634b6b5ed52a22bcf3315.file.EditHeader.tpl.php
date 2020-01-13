893e85a2acd334f62d99006c9f8b3037a870719a
sWIm-crm <sWIm-crm@protonmail.com>
2020-01-13 11:10:14 +0000

    Config backup

---
+++ test/templates_c/v7/0c22939721d673eaec5634b6b5ed52a22bcf3315.file.EditHeader.tpl.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2020-01-13 11:09:32
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Settings/Workflows/EditHeader.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:8119414975e1c4fecd45052-37556973%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '0c22939721d673eaec5634b6b5ed52a22bcf3315' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Settings/Workflows/EditHeader.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '8119414975e1c4fecd45052-37556973',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5e1c4fece12c1',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5e1c4fece12c1')) {function content_5e1c4fece12c1($_smarty_tpl) {?>
+
+
+
+<?php }} ?>
