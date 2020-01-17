fc711c6db2fb0429fbddd223e2b72c97c0cd1d30
sWIm-crm <sWIm-crm@protonmail.com>
2020-01-17 22:41:43 +0000

    Config backup

---
+++ test/templates_c/v7/dff44d69696e36cb43518c79ffec78daae8fe2dc.file.TagCloud.tpl.php
@@ -0,0 +1,30 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2020-01-17 22:41:40
+         compiled from "/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Vtiger/dashboards/TagCloud.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:3604866925e2238243f4412-42545039%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'dff44d69696e36cb43518c79ffec78daae8fe2dc' => 
+    array (
+      0 => '/var/www/html/mastercrm-vtiger/includes/runtime/../../layouts/v7/modules/Vtiger/dashboards/TagCloud.tpl',
+      1 => 1560717990,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '3604866925e2238243f4412-42545039',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'MODULE_NAME' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_5e223824435bb',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5e223824435bb')) {function content_5e223824435bb($_smarty_tpl) {?>
+<div class="dashboardWidgetHeader"><?php echo $_smarty_tpl->getSubTemplate (vtemplate_path("dashboards/WidgetHeader.tpl",$_smarty_tpl->tpl_vars['MODULE_NAME']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+</div><div class="dashboardWidgetContent" style='padding:5px'><?php echo $_smarty_tpl->getSubTemplate (vtemplate_path("dashboards/TagCloudContents.tpl",$_smarty_tpl->tpl_vars['MODULE_NAME']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+</div><div class="widgeticons dashBoardWidgetFooter"><div class="footerIcons pull-right"><?php echo $_smarty_tpl->getSubTemplate (vtemplate_path("dashboards/DashboardFooterIcons.tpl",$_smarty_tpl->tpl_vars['MODULE_NAME']->value), $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
+</div></div>	 <?php }} ?>
